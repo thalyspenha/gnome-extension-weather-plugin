22497798cb2999a653945d14975ae7473a44fb03
Thalys Valisi <thalysvalisi@gmail.com>
2026-06-12 10:19:10 -0300

    feat: expand WeatherModel with precipitation and wind gust fields

--- data/WeatherModel.js
+++ data/WeatherModel.js
@@ -1,24 +1,62 @@
 /**
+ * @typedef {Object} OpenMeteoRawResponse
+ * @property {{
+ *   temperature_2m:         number,
+ *   apparent_temperature:   number,
+ *   weathercode:            number,
+ *   windspeed_10m:          number,
+ *   windgusts_10m:          number,
+ *   relativehumidity_2m:    number,
+ *   precipitation:          number,
+ * }} current
+ * @property {{
+ *   time:                      string[],
+ *   temperature_2m:            number[],
+ *   weathercode:               number[],
+ *   precipitation_probability: number[],
+ *   precipitation:             number[],
+ * }} hourly
+ * @property {{
+ *   time:                          string[],
+ *   weathercode:                   number[],
+ *   temperature_2m_max:            number[],
+ *   temperature_2m_min:            number[],
+ *   precipitation_sum:             number[],
+ *   precipitation_probability_max: number[],
+ *   windspeed_10m_max:             number[],
+ *   windgusts_10m_max:             number[],
+ * }} daily
+ */
+
+/**
  * @typedef {Object} CurrentWeather
  * @property {number} temperature
  * @property {number} apparentTemperature
  * @property {number} humidity
- * @property {number} windSpeed
+ * @property {number} windSpeed           km/h
+ * @property {number} windGust            km/h
+ * @property {number} precipitation       mm
  * @property {number} weatherCode
  */
 
 /**
  * @typedef {Object} HourlySlot
- * @property {string} time        ISO 8601
+ * @property {string} time               ISO 8601
  * @property {number} temperature
+ * @property {number} precipitationProb  0–100 %
+ * @property {number} precipitation      mm
  * @property {number} weatherCode
  */
 
 /**
  * @typedef {Object} DailyDay
- * @property {string} date        YYYY-MM-DD
+ * @property {string} date              YYYY-MM-DD
  * @property {number} tempMax
  * @property {number} tempMin
+ * @property {number} precipitationSum  mm
+ * @property {number} precipitationProb 0–100 %
+ * @property {number} windSpeedMax      km/h
+ * @property {number} windGustMax       km/h
  * @property {number} weatherCode
  */
 
